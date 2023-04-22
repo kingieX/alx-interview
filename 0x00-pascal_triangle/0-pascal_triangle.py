c498951623033132a41ee5d92c89acf026e10cba
Nnachi Kingsley <chimakingsley2162gmail.com>
2023-04-22 11:19:50 +0000

    successfully tested pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+returns a list of lists of integers representing pascal triangle
+"""
+def pascal_triangle(n):
+    # if n is less than or equal to 0, return an empty list
+    if n <= 0:
+        return []
+
+    row = [1]
+
+    result = [row]
+
+    for i in range(1, n):
+        """
+        generate the next row by appending a 0 to the end of the previous row
+        and then adding it element-wise to the previous row shifted 
+        one position to the right
+        """
+        row = [x+y for x,y in zip(row + [0], [0] + row)]
+        result.append(row)
+
+    return result
