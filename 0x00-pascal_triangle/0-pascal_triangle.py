2dd4abfa9337c629d5544384356f0fce48f36095
Nnachi Kingsley <chimakingsley2162gmail.com>
2023-04-22 12:12:59 +0000

    successfully tested pascal's triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,10 +3,11 @@
 The pascal's triangle
 """
 
+
 def pascal_triangle(n):
     """
-    Function that returns a list of lists of integers 
-    representing the pascal's triangle of n.
+        Function that returns a list of lists of integers 
+        representing the pascal's triangle of n.
     """
     if n <= 0:
         return []
@@ -14,5 +15,5 @@ def pascal_triangle(n):
     result = [row]
     for i in range(1, n):
         row = [x+y for x,y in zip(row + [0], [0] + row)]
-        result.append(row)
+    result.append(row)
     return result
