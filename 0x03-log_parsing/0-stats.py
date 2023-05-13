3d413d62fb780c4d92cf5835b623f12cf807016c
Nnachi Kingsley <chimakingsley2162gmail.com>
2023-05-13 13:13:35 +0000

    commited

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,15 +1,42 @@
 #!/usr/bin/python3
-import random
-import sys
-from time import sleep
-import datetime
+"""
+Log parsing with metrics computation
+"""
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+import sys
+import re
+
+if __name__ == '__main__':
+    line_count = 0
+    total_file_size = 0
+    status_codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {code: 0 for code in status_codes}
+
+    def print_stats(stats: dict, total_size: int) -> None:
+        print("Total file size:", total_size)
+        for code in sorted(stats):
+            if stats[code] > 0:
+                print(code + ":", stats[code])
+
+    try:
+        for line in sys.stdin:
+            line_count += 1
+            line = line.strip()
+
+            match = re.match(r'.*GET \/projects\/260 HTTP\/1\.1" (\d+) (\d+)', line)
+            if match:
+                status_code = match.group(1)
+                file_size = int(match.group(2))
+
+                if status_code in status_codes:
+                    stats[status_code] += 1
+                total_file_size += file_size
+
+            if line_count % 10 == 0:
+                print_stats(stats, total_file_size)
+
+        print_stats(stats, total_file_size)
+
+    except KeyboardInterrupt:
+        print_stats(stats, total_file_size)
+        raise
