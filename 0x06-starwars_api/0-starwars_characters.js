cf783f25c22b270f64e5cc5ef0312b48344e9b16
Nnachi Kingsley <chimakingsley2162gmail.com>
2023-06-02 13:50:16 +0000

    run semistandard fix

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,36 +4,35 @@ const request = require('request');
 const movieId = process.argv[2];
 
 request(`https://swapi.dev/api/films/${movieId}/`, function (error, response, body) {
-	if (error) {
-		console.error('Error:', error);
-	} else if (response.statusCode !== 200) {
-		console.error('Unexpected status code:', response.statusCode);
-	} else {
-		const film = JSON.parse(body);
-		const characters = film.characters;
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Unexpected status code:', response.statusCode);
+  } else {
+    const film = JSON.parse(body);
+    const characters = film.characters;
 
-		fetchCharacters(characters, 0);
-	}
+    fetchCharacters(characters, 0);
+  }
 });
 
-function fetchCharacters(characters, index) {
-	if (index >= characters.length) {
-		return;
-	}
+function fetchCharacters (characters, index) {
+  if (index >= characters.length) {
+    return;
+  }
 
+  const characterUrl = characters[index];
 
-	const characterUrl = characters[index];
+  request(characterUrl, function (error, response, body) {
+    if (error) {
+      console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+      console.error('Unexpectedstatus code:', response.statusCode);
+    } else {
+      const character = JSON.parse(body);
+      console.log(character.name);
 
-	request(characterUrl, function (error, response, body) {
-		if (error) {
-			console.error('Error:', error);
-		} else if (response.statusCode !== 200) {
-			console.error('Unexpectedstatus code:', response.statusCode);
-		} else {
-			const character = JSON.parse(body);
-			console.log(character.name);
-
-			fetchCharacters(characters, index + 1);
-		}
-	});
+      fetchCharacters(characters, index + 1);
+    }
+  });
 }
