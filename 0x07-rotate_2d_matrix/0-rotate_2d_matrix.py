c6cf7fd8255b521ebce9361d0166efd7b65e9563
nnachi kingsley <chimakingsley216@gmail.com>
2023-06-10 12:31:58 +0000

    resolve pycodestyle test

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -2,18 +2,23 @@
 """
 2D Matrix
 """
+
+
 def rotate_2d_matrix(matrix):
     """
     Rotate a 2D matrix 90 degrees clockwise
+
     Args:
         matrix (list): n x n. Matrix to rotate
     """
     n = len(matrix)
 
     for i in range(n):
-        for j in range(i+1, n):
-          matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for j in range(i + 1, n):
+            matrix[i][j] = matrix[j][i]
+            matrix[j][i] = matrix[i][j]
 
     for i in range(n):
         for j in range(n // 2):
-            matrix[i][j], matrix[i][n - 1 - j] = matrix[i][n - 1 - j], matrix[i][j]
+            matrix[i][j] = matrix[i][n - 1 - j]
+            matrix[i][n - 1 - j] = matrix[i][j]
