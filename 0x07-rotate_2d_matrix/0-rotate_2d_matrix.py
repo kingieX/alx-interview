c21587e1252dc824031e95c1b1519d2527fc3bde
nnachi kingsley <chimakingsley216@gmail.com>
2023-06-10 12:35:52 +0000

    resolve pycodestyle test

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 """
-2D Matrix
-"""
-
+2D Matrix"""
 
 def rotate_2d_matrix(matrix):
     """
@@ -14,11 +12,9 @@ def rotate_2d_matrix(matrix):
     n = len(matrix)
 
     for i in range(n):
-        for j in range(i + 1, n):
-            matrix[i][j] = matrix[j][i]
-            matrix[j][i] = matrix[i][j]
+        for j in range(i+1, n):
+          matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
     for i in range(n):
         for j in range(n // 2):
-            matrix[i][j] = matrix[i][n - 1 - j]
-            matrix[i][n - 1 - j] = matrix[i][j]
+            matrix[i][j], matrix[i][n - 1 - j] = matrix[i][n - 1 - j], matrix[i][j]
